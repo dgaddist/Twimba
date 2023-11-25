de9b340519f45749d2f2ceaf7f15f112484f7e4e
dgaddist <128024246+dgaddist@users.noreply.github.com>
2023-11-25 01:45:41 -0800

    add handleLikeClick func

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ tweetBtn.addEventListener("click", function () {
   console.log(tweetInput.value);
 });
 document.addEventListener("click", function (e) {
-  console.log(e.target.dataset.like);
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  }
 });
 
+function handleLikeClick(tweetId) {
+  console.log(tweetId);
+}
 function getFeedHtml() {
   let feedHtml = ``;
   tweetsData.forEach(function (tweet) {
