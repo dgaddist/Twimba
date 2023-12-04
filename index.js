a20709a763cbbf92500ba9b4358424a627b0c93a
dgaddist <128024246+dgaddist@users.noreply.github.com>
2023-12-03 21:16:05 -0800

    add handle retweet click function and else if statement to event listener

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ tweetBtn.addEventListener("click", function () {
 document.addEventListener("click", function (e) {
   if (e.target.dataset.like) {
     handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
   }
 });
 
@@ -25,6 +27,21 @@ function handleLikeClick(tweetId) {
   targetTweetObj.isLiked = !targetTweetObj.isLiked;
   render();
 }
+
+function handleRetweetClick(tweetId) {
+  const retweetTweetObj = tweetsData.filter(function (retweet) {
+    return retweet.uuid === tweetId;
+  })[0];
+
+  if (retweetTweetObj.isRetweeted) {
+    retweetTweetObj.retweets--;
+  } else {
+    retweetTweetObj.retweets++;
+  }
+  retweetTweetObj.isRetweeted = !retweetTweetObj.isRetweeted;
+  render();
+}
+
 function getFeedHtml() {
   let feedHtml = ``;
   tweetsData.forEach(function (tweet) {
