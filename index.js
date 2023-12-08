eb21380e7a1eddc4a9737ed15e33ff42259b9c54
dgaddist <128024246+dgaddist@users.noreply.github.com>
2023-12-07 22:47:08 -0800

    add if statement to console log tweet uuid of  tweets w/ comment

--- index.js
+++ index.js
@@ -51,6 +51,16 @@ function getFeedHtml() {
       likeIconClass = "liked";
     }
 
+    let retweetIconClass = "";
+
+    if (tweet.isRetweeted) {
+      retweetIconClass = "liked";
+    }
+    let repliesHtml = "";
+
+    if (tweet.replies.length > 0) {
+      console.log(tweet.uuid);
+    }
     feedHtml += `
   <div class="tweet">
     <div class="tweet-inner">
@@ -70,7 +80,7 @@ function getFeedHtml() {
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                 <i class="fa-solid fa-retweet"
+                 <i class="fa-solid fa-retweet ${retweetIconClass}"
                  data-retweet="${tweet.uuid}"></i>
                     ${tweet.retweets}
                 </span>
